52d6d455e2bb8540cad32e8926b3d701764de7d5
pmk1111 <kingpmk104@naver.com>
2024-09-23 17:06:12 +0900

    ui 수정 & 주석 제거

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,19 +1,17 @@
 import React, { useContext, useRef, useCallback } from "react";
 import { GalleryContext } from "../app/provider/galleryContext";
 import { slideItem } from "../features/slideItem";
-import "../app/styles/gallery.css";
-
 
 function Gallery() {
   const galleryDatas = useContext(GalleryContext);
   const galleryRef = useRef();
-  const debounceTimer = useRef(null); // debounce 타이머 저장
+  const debounceTimer = useRef(null); 
 
   const handleClick = useCallback((direction) => {
-    if (debounceTimer.current) return; // 이미 타이머가 설정된 경우 무시
+    if (debounceTimer.current) return;
 
     slideItem(galleryRef.current, galleryDatas.length, direction);
-    
+
     // 0.6초 후 타이머 초기화
     debounceTimer.current = setTimeout(() => {
       debounceTimer.current = null;
@@ -21,19 +19,25 @@ function Gallery() {
   }, []);
 
   return (
-    <div className="gallery-slider">
-      <div className="flex align-center">
-        <button className="slide-btn left" onClick={() => handleClick('prev')}>&lt;</button>
-        <div className="gallery-wrapper overflow">
-          <div className="gallery-items" ref={galleryRef}>
-            {galleryDatas.map((gallery) => (
-              <div key={gallery.id} className="slide">
-                <img src={require("../imgs/" + gallery.imgSrc)} alt={gallery.id}></img>
-              </div>
-            ))}
+    <div>
+      <div className="gallery-slider">
+        <div className="flex align-center j-f-center">
+          <div className="gallery-wrapper overflow">
+            <div className="gallery-items" ref={galleryRef}>
+              {galleryDatas.map((gallery) => (
+                <div key={gallery.id} className="slide">
+                  <img src={require("../imgs/" + gallery.imgSrc)} alt={gallery.imgSrc}></img>
+                </div>
+              ))}
+            </div>
           </div>
+
         </div>
-        <button className="slide-btn right" onClick={() => handleClick('next')}>&gt;</button>
+        <div className="overlay"></div>
+      </div>
+      <div className="slide-btn-container">
+        <button className="slide-btn left" onClick={() => handleClick("prev")}>&lt;</button>
+        <button className="slide-btn right"  onClick={() => handleClick("next")}>&gt;</button>
       </div>
     </div>
   );
