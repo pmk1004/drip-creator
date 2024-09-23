8c44bb4fc0250f902b0a073fc7d835156cbbaa88
pmk1111 <kingpmk104@naver.com>
2024-09-23 17:12:31 +0900

    드립 & 명언 생성 함수 쿨타임 추가

--- src/features/getDrip.js
+++ src/features/getDrip.js
@@ -1,8 +1,16 @@
 import { createRandomNum } from "./createRandomNum";
 
-export function getDrip(drips, setDrip) {
-  const idx = createRandomNum(drips.length);
-  console.log(drips[idx].drip);
+export function getDrip(drips, setDrip, cooldownRef) {
+  if (!cooldownRef.current) {
+    const idx = createRandomNum(drips.length);
+    console.log(drips[idx].drip);
 
-  setDrip(drips[idx].drip);
+    setDrip(drips[idx].drip);
+    
+    // 쿨타임 설정
+    cooldownRef.current = true;
+    setTimeout(() => {
+      cooldownRef.current = false;
+    }, 600); // 0.6초
+  }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,14 @@
-import React, { useState, useEffect, useLayoutEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useCallback, useRef } from "react";
 import { GalleryContext } from "../app/provider/galleryContext";
 import { createRandomNum } from "../features/createRandomNum";
 import { getDrip } from "../features/getDrip";
-import "../app/styles/home.css";
 
 function Home(props) {
   const [num, setNum] = useState();
   const [drip, setDrip] = useState('여기에 명언이 표시됩니다.');
   const lightDrips = props.drips[0];
   const heavyDrips = props.drips[1];
+  const debounceTimer = useRef(null);
 
   const galleryData = useContext(GalleryContext);
 
@@ -16,6 +16,17 @@ function Home(props) {
     const idx = createRandomNum(galleryData.length)
     setNum(idx);
     console.log("setNum 실행");
+  }, []);
+  
+  const handleClick = useCallback(() => {
+    if (debounceTimer.current) return; // 이미 타이머가 설정된 경우 무시
+
+    setNum(createRandomNum(galleryData.length));
+
+    // 0.6초 후 타이머 초기화
+    debounceTimer.current = setTimeout(() => {
+      debounceTimer.current = null;
+    }, 500);
   }, []);
 
   const data = num !== undefined ? galleryData[num] : galleryData[0];
@@ -32,7 +43,7 @@ function Home(props) {
           </div>
           <div className="overlay"></div>
         </div>
-        <button onClick={() => setNum(createRandomNum(galleryData.length))}>
+        <button onClick={handleClick}>
           이미지 변경
         </button>
       </div>
@@ -41,10 +52,10 @@ function Home(props) {
       <div className="drip-container m-t-25">
         <span className="f-25 block center m-b-25">명언 생성하기</span>
         <div className="btn-wrapper">
-          <button onClick={() => getDrip(lightDrips, setDrip)}>
+          <button onClick={() => getDrip(lightDrips, setDrip, debounceTimer)}>
             명언 생성(light)
           </button>
-          <button onClick={() => getDrip(heavyDrips, setDrip)}>
+          <button onClick={() => getDrip(heavyDrips, setDrip, debounceTimer)}>
             명언 생성(heavy)
           </button>
         </div>
