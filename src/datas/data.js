29bf511787e964c73c200928ed7c0a6eec4d92c7
pmk1111 <kingpmk104@naver.com>
2024-09-26 14:40:14 +0900

    s

---
+++ src/datas/data.js
@@ -0,0 +1,23 @@
+export const galleryData = [
+    {id: 1, drip: "머리가 안 좋은데 왜 니 애비보다 연봉이 10배 이상 높을까?", author: "한주먹", imgSrc: "한주먹_1.jpg"},
+    {id: 2, drip: "ㅇ미뒤진 새끼들 이악물고 악플달러 오는거 존나 웃기네ㅋㅋㅋ", author: "한주먹", imgSrc: "한주먹_2.jpg"},
+    {id: 3, drip: "니 ㅇ미가 너 꼴뵈기 싫어하는 것 보다 더할까ㅋㅋ", author: "한주먹", imgSrc: "한주먹_3.png"},
+    {id: 4, drip: "3일 안에 니 ㅇ미 뒤지라고 오늘부터 제사지냄", author: "한주먹", imgSrc: "한주먹_4.png"},
+    {id: 5, drip: "칼로 니 친정 엄마 x발년아, x구멍 찢어서 x탱이에...", author: "이재명", imgSrc: "이재명-2.jpg"},
+    {id: 6, drip: "하...마이머머님...왜 피임을 안하셨어요...", author: "괴물쥐", imgSrc: "괴물쥐-1.jpeg"},
+    {id: 7, drip: "네 부모가 친부모인지부터 의심해라. 고아원 원장이랑 주작했을지 누가 아는데", author: "강한달", imgSrc: "강한달-1.jpg"},
+    {id: 8, drip: "구제역 너네 얼마나 너가 역겨우면 너의 어머니가 스트레스 받아서 안타깝게 일찍 돌아가시냐?", author: "이근", imgSrc: "이근-1.jpg"},
+    {id: 9, drip: "넌 니ㅇ미 따라가라", author: "가재맨", imgSrc: "가재맨-2.gif"},
+    {id: 10, drip: "페이커 ㅇ미 창ㄴ!!!!!", author: "가재맨", imgSrc: "가재맨-3.jpg"},
+    {id: 11, drip: "커밍아웃 후 부모의 ㅈ같음 견디는 꿀팁좀 알려줘", author: "한국여성인권진흥원 공식 트위터 중...", imgSrc: "한녀진흥원.jpg"},
+  ];
+  
+  export const lightDrip = [
+    {drip: "니 엄마 인중냄새 방구냄새"},
+    {drip: "니 아빠 발가락 무좀 있음"}
+  ]
+  
+  export const heavyDrip = [
+    {drip: "애미 손잡고 장애인들이랑 재활센터에서 보x 재활이나 시켜드려라"},
+    {drip: "니 애미 x지 실험용 침팬치 성용 극에 달했을 때 진정시켜줄라고 대주는 혹성탈출 보x"}
+  ]
